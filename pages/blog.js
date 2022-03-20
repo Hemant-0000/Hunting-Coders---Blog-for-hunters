15804bb660fa586bda2467eda6d23c33c88a5d54
Hemant <hrsmart2150@gmail.com>
2022-03-20 13:07:49 +0530

    Creating Blog and Blogpost page and added their own css files

--- pages/blog.js
+++ pages/blog.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import styles from '../styles/Blog.module.css'
+import Link from 'next/link'
+
 
 const Blog = () => {
   return (
-    <div>
-        This is blog component
-    </div>
+    <>
+      <div className={styles.container}>
+        <main className={styles.main}>
+
+          <div className={styles.blogItem}>
+            <Link passHref href={'/blogpost/learn-javascript'}>
+              <h3>How to learn JavaScript in 2022 ?</h3>
+            </Link>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam reiciendis distinctio explicabo...</p>
+          </div>
+
+          <div className={styles.blogItem}>
+            <h3>How to learn JavaScript in 2022 ?</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam reiciendis distinctio explicabo...</p>
+          </div>
+
+          <div className={styles.blogItem}>
+            <h3>How to learn JavaScript in 2022 ?</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam reiciendis distinctio explicabo...</p>
+          </div>
+
+        </main>
+      </div>
+    </>
   )
 }
 
--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,10 +1,24 @@
 import React from 'react'
 import { useRouter } from 'next/router'
+import styles from '../../styles/BlogPost.module.css'
+
 
 const Slug = () => {
     const router = useRouter();
     const { slug } = router.query
-    return <div> {slug} </div>
+    return (
+        <>
+            <div className={ styles.container }>
+                <main className={ styles.main }>
+                <h1>Title of the page {slug} </h1>
+                    <hr />
+                    <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, dolorum enim, corporis necessitatibus doloremque eveniet maiores distinctio, excepturi soluta beatae animi autem suscipit.
+                    </p>
+                </main>
+            </div>
+        </>
+    )
 }
 
 export default Slug
