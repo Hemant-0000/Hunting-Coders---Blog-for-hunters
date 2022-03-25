7e6e8c223505b86dfd8559691b805048d245514c
Hemant <hrsmart2150@gmail.com>
2022-03-25 18:50:09 +0530

    Using the API Endpoints in the frontend to fetching blogs

--- pages/blog.js
+++ pages/blog.js
@@ -1,22 +1,39 @@
 import React from 'react'
 import styles from '../styles/Blog.module.css'
+import { useEffect } from 'react/cjs/react.development'
+import { useState } from 'react/cjs/react.development'
 import Link from 'next/link'
 
 
 // Step 1: Collect all the files data from blogdata directory
 // Step 2: Iterate through the and display them
 const Blog = () => {
+
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/blogs').then((a) => {
+      return a.json();
+    })
+      .then((parsed) => {
+        setBlogs(parsed)
+      })
+  }, [])
+
   return (
     <>
       <div className={styles.container}>
         <main className={styles.main}>
 
-          <div className={styles.blogItem}>
-            <Link passHref href={'/blogpost/learn-javascript'}>
-              <h3>How to learn JavaScript in 2022 ?</h3>
-            </Link>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam reiciendis distinctio explicabo...</p>
-          </div>
+          {blogs.map((blogItem) => {
+            return <div key={blogItem.slug} className={styles.blogItems}>
+              <Link passHref href={`/blogpost/${blogItem.slug}`}>
+                <h3>{blogItem.title}</h3>
+              </Link>
+              <p className={styles.blogItemP}>{blogItem.content.substr(0, 120)}...</p>
+            </div>
+          })}
+
         </main>
       </div>
     </>
